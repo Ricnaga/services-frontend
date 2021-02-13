679725e0e4e7066a7d4c9184f7f4115a6dd82128
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-02-13 17:17:51 -0300

    add input|button components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons/lib';
+import { Container } from './style';
+
+interface Inputprops extends InputHTMLAttributes<HTMLInputElement>{
+  name:string;
+  icon?:React.ComponentType<IconBaseProps>;
+}
+
+const Input:React.FC<Inputprops> = ({ icon: Icon, ...rest }) => (
+  <Container>
+    {Icon && <Icon /> }
+    <input type="text" {...rest} />
+  </Container>
+);
+
+export default Input;
