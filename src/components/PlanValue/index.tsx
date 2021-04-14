75f1f5f024c960a4c3f8ee5cc562679715ba8983
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-04-14 00:50:08 -0300

    eslint to components | created component planvalue

---
+++ src/components/PlanValue/index.tsx
@@ -0,0 +1,15 @@
+interface PlanValueProps {
+  basicValue: number;
+  individualValue: number;
+}
+
+export default function PlanValue({ basicValue, individualValue }: PlanValueProps) {
+  return (
+    <h4>
+      Valor do plano: R$
+      {basicValue + individualValue > 120
+        ? basicValue + individualValue * 0.6
+        : basicValue + individualValue}
+    </h4>
+  );
+}
