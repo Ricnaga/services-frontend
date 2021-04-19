55cc9f9540400f7db7d8513ddb3a1e2a95dc2c4f
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-04-19 00:27:01 -0300

    refactoring changedata page | missing handleform

--- src/pages/ChangeData/index.tsx
+++ src/pages/ChangeData/index.tsx
@@ -1,23 +1,21 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import {
-  HiOutlineDocument, HiOutlineDocumentReport,
-  HiOutlineGlobe, HiOutlineMail, HiOutlineUser,
+  HiOutlineDocument,
+  HiOutlineDocumentReport,
+  HiOutlineGlobe,
+  HiOutlineMail,
+  HiOutlineUser,
 } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PlanValue from '../../components/PlanValue';
 import api from '../../services/api';
-import {
-  Container,
-  Form,
-  GymPlan,
-  Label, SearchContent,
-} from './style';
+import { Container, Form, GymPlan, Label, SearchContent } from './style';
 
 interface AllPlans {
   workout: string;
-  servicePackage: string;
+  servicePlan: 'Basic' | 'Individual';
 }
 
 interface UserResponse {
@@ -27,227 +25,225 @@ interface UserResponse {
   address: string;
   email: string;
   account: boolean;
-}
-
-interface FormData {
-  id: string;
-  name: string;
-  rg: string;
-  address: string;
-  email: string;
   userPlan: AllPlans[];
-  account: boolean;
 }
 
-const ChangeData: React.FC = () => {
-  const [newInput, setNewInput] = useState('');
-  const [newBasicCheckedInput, setNewBasicCheckedInput] = useState(false);
-  const [userFound, setUserFound] = useState('');
-  const [userAccount, setUserAccount] = useState(false);
-  const [newUser, setnewUser] = useState<UserResponse>();
+export default function ChangeData() {
   const [plan, setPlan] = useState<AllPlans[]>([]);
-  const [individualPlan, setIndividualPlan] = useState<AllPlans[]>([]);
-  const [paymentBasicValue, setPaymentBasicValue] = useState(0);
-  const [paymentIndividualValue, setPaymentIndividualValue] = useState(0);
-  const [servicePackage, setServicePackage] = useState<AllPlans[]>([]);
+  const [basicValue, setBasicValue] = useState(0);
+  const [individualValue, setIndividualValue] = useState(0);
+  const [servicePlan, setServicePlan] = useState<AllPlans[]>([]);
+  const [findUserInput, setFindUserInput] = useState('');
+  const [userBasicPlan, setUserBasicPlan] = useState(false);
+  const [userIndividualPlan, setUserIndividualPlan] = useState([]);
+  const [userNotFound, setUserNotFound] = useState('');
+  const [userFound, setUserFound] = useState<UserResponse>();
 
   useEffect(() => {
-    api.get('plans/show').then((response) => {
+    api.get('plans/show').then(response => {
       setPlan(response.data.plans);
     });
   }, []);
 
   const handleSearch = useCallback(async () => {
-    setnewUser(undefined);
-    setUserAccount(false);
+    const findUser = await api.put('users/find', findUserInput);
 
-    const foundUser = await api.put('users/find', newInput);
-
-    if (!foundUser.data[0]) {
-      setUserFound('Usuário não encontrado');
+    if (!findUser.data[0]) {
+      setUserNotFound('Usuário não encontrado');
     } else {
-      setnewUser(foundUser.data[0]);
-      const { userPlan } = foundUser.data[0];
-      const userOnlyIndividualPlans: AllPlans[] = [];
+      setUserFound(findUser.data[0]);
+      const { userPlan } = findUser.data[0];
 
-      userPlan.filter((plans:AllPlans) => (plans.servicePackage === 'Basic'
-        ? setNewBasicCheckedInput(true)
-        : userOnlyIndividualPlans.push(plans)));
+      const findUserIndividualPlans = userPlan
+        .filter((userPlans: AllPlans) =>
+          userPlans.servicePlan !== 'Basic'
+            ? userPlans
+            : (setUserBasicPlan(true), setBasicValue(80)),
+        )
+        .map((p: AllPlans) => p.workout);
 
-      setIndividualPlan(userOnlyIndividualPlans);
-
-      if (!newBasicCheckedInput) {
-        setServicePackage([...servicePackage, {
-          workout: 'Basic',
-          servicePackage: 'Basic',
-        }]);
-        setPaymentBasicValue(paymentBasicValue + 80);
-      } else {
-        const service = servicePackage.filter((svc) => svc.workout !== 'Basic');
-        setServicePackage(service);
-        setPaymentBasicValue(paymentBasicValue - 80);
-      }
+      setUserIndividualPlan(findUserIndividualPlans);
+      setIndividualValue(120 * findUserIndividualPlans.length);
     }
-  }, [newInput, newBasicCheckedInput, servicePackage, paymentBasicValue]);
+  }, [findUserInput]);
 
-  const handleValue = useCallback((event) => {
-    if (event.target.name === 'Basic') {
-      if (event.target.checked) {
-        const newWorkout = {
-          workout: event.target.name,
-          servicePackage: 'Basic',
-        };
-        setServicePackage([...servicePackage, newWorkout]);
-        setPaymentBasicValue(paymentBasicValue + 80);
-      } else {
-        const service = servicePackage.filter((svc) => svc.workout !== event.target.name);
-        setServicePackage(service);
-        setPaymentBasicValue(paymentBasicValue - 80);
+  function checkIndividualPlans(plans: AllPlans) {
+    const checkingPlans = userIndividualPlan.filter(
+      individual => plans.workout === individual,
+    );
+
+    return checkingPlans[0];
+  }
+
+  const handleValue = useCallback(
+    event => {
+      const { name, checked } = event.target;
+
+      if (name === 'Basic') {
+        if (checked) {
+          const findBasicPlan = servicePlan.filter(
+            service => service.servicePlan === 'Basic',
+          );
+          const addedBasicPlan = plan.filter(p => p.servicePlan === 'Basic');
+          setBasicValue(80);
+
+          if (servicePlan.length && !findBasicPlan.length) {
+            setServicePlan([...servicePlan, ...addedBasicPlan]);
+          }
+
+          if (!servicePlan.length) {
+            setServicePlan(addedBasicPlan);
+          }
+        } else {
+          const removedBasicPlan = servicePlan.filter(
+            p => p.servicePlan !== 'Basic',
+          );
+          setServicePlan(removedBasicPlan);
+          setBasicValue(0);
+        }
       }
-    }
 
-    if (event.target.name === 'Individual') {
-      if (event.target.checked) {
-        const newWorkout = {
-          workout: event.target.name,
-          servicePackage: 'Individual',
-        };
-        setServicePackage([...servicePackage, newWorkout]);
-        setPaymentIndividualValue(paymentIndividualValue + 120);
-      } else {
-        const service = servicePackage.filter((svc) => svc.workout !== event.target.name);
-        setServicePackage(service);
-        setPaymentIndividualValue(paymentIndividualValue - 120);
+      if (name !== 'Basic') {
+        if (checked) {
+          const findIndividualPlan = servicePlan.filter(
+            service => service.workout === name,
+          );
+          const addedIndividualPlan = plan.filter(p => p.workout === name);
+          setIndividualValue(individualValue + 120);
+
+          if (servicePlan.length && !findIndividualPlan.length) {
+            setServicePlan([...servicePlan, ...addedIndividualPlan]);
+          }
+          if (!servicePlan.length) {
+            setServicePlan(addedIndividualPlan);
+          }
+        } else {
+          const removedIndividualPlan = servicePlan.filter(
+            p => p.workout !== name,
+          );
+          setServicePlan(removedIndividualPlan);
+          setIndividualValue(individualValue - 120);
+        }
       }
-    }
-
-    if (event.target.name === 'account') {
-      if (event.target.checked) {
-        setUserAccount(true);
-      } else {
-        setUserAccount(false);
-      }
-    }
-  }, [paymentBasicValue, paymentIndividualValue, servicePackage]);
-
-  const handleForm = useCallback(
-    async (event) => {
-      event.preventDefault();
-
-      const formValue: FormData = {
-        id: newInput,
-        name: event.target[0].value,
-        rg: event.target[1].value,
-        address: event.target[2].value,
-        email: event.target[3].value,
-        userPlan: servicePackage,
-        account: userAccount,
-      };
-      await api.put('/users/update', formValue);
     },
-    [newInput, servicePackage, userAccount],
+    [individualValue, plan, servicePlan],
   );
+
+  const handleForm = useCallback(async event => {
+    event.preventDefault();
+    // console.log(event.target[5].checked); - campo check do basic
+    console.log(event.target[6].elements[0].name);
+  }, []);
 
   return (
-    <>
-      <Container>
-        <h2>Alteração de dados cadastrais</h2>
-        <SearchContent>
+    <Container>
+      <h2>Alteração de dados cadastrais</h2>
+      <SearchContent>
+        <Input
+          icon={HiOutlineDocumentReport}
+          name="id"
+          onChange={e => setFindUserInput(e.target.value)}
+        />
+        <Button type="button" onClick={() => handleSearch()}>
+          Buscar
+        </Button>
+      </SearchContent>
+
+      {userFound ? (
+        <Form onSubmit={handleForm}>
           <Input
-            name="id"
-            value={newInput}
-            onChange={(e) => setNewInput(e.target.value)}
-            icon={HiOutlineDocumentReport}
-            placeholder="Digite o Id"
+            name="name"
+            icon={HiOutlineUser}
+            placeholder="Nome"
+            defaultValue={userFound.name}
           />
-          <Button type="button" onClick={handleSearch}>Buscar</Button>
-        </SearchContent>
-
-        {newUser
-          ? (
-            <Form onSubmit={handleForm}>
-              <Input
-                name="name"
-                icon={HiOutlineUser}
-                placeholder="Nome"
-                defaultValue={newUser.name}
-              />
-              <Input
-                name="rg"
-                icon={HiOutlineDocument}
-                placeholder="RG"
-                defaultValue={newUser.rg}
-              />
-              <Input
-                name="address"
-                icon={HiOutlineGlobe}
-                placeholder="Endereço"
-                defaultValue={newUser.address}
-              />
-              <Input
-                name="email"
-                icon={HiOutlineMail}
-                placeholder="E-mail"
-                defaultValue={newUser.email}
-              />
-              <GymPlan>
-                <fieldset>
-                  <legend>
-                    {plan.map((modality) => modality.servicePackage === 'Basic')
-                      && (
-                        <Input
-                          name="Basic"
-                          type="checkbox"
-                          checked={newBasicCheckedInput}
-                          onChange={(e) => setNewBasicCheckedInput(e.target.checked)}
-                          onClick={handleValue}
-                        />
-                      )}
-
-                    Básico
-                  </legend>
-                  {plan.map((modality) => (
-                    modality.servicePackage === 'Basic'
-                    && (
-                      <Label key={modality.workout}>
-                        {modality.workout}
-                      </Label>
-                    )
-                  ))}
-                </fieldset>
-
-                <fieldset>
-                  <legend>Individual</legend>
-
-                  {plan.map((modality) => (
-                    modality.servicePackage === 'Individual'
-                    && (
-                      <Label key={modality.workout}>
-                        <Input name={modality.workout} onClick={handleValue} type="checkbox" />
-                        {modality.workout}
-                      </Label>
-                    )))}
-
-                </fieldset>
-              </GymPlan>
-              <Label>
+          <Input
+            name="rg"
+            icon={HiOutlineDocument}
+            placeholder="RG"
+            defaultValue={userFound.rg}
+          />
+          <Input
+            name="address"
+            icon={HiOutlineGlobe}
+            placeholder="Endereço"
+            defaultValue={userFound.address}
+          />
+          <Input
+            name="email"
+            icon={HiOutlineMail}
+            placeholder="E-mail"
+            defaultValue={userFound.email}
+          />
+          <GymPlan>
+            <fieldset>
+              <legend>
                 <Input
-                  name="account"
+                  name="Basic"
                   type="checkbox"
-                  defaultChecked={newUser.account}
+                  checked={userBasicPlan}
+                  onChange={e => setUserBasicPlan(e.target.checked)}
                   onClick={handleValue}
                 />
-                Conta ativa ?
-              </Label>
-              <PlanValue basicValue={paymentBasicValue} individualValue={paymentIndividualValue} />
-              <Button type="submit">Enviar</Button>
-            </Form>
-          ) : <h2>{userFound}</h2>}
+                Básico
+              </legend>
+              {plan.map(
+                basicPlan =>
+                  basicPlan.servicePlan === 'Basic' && (
+                    <Label key={basicPlan.workout}>{basicPlan.workout}</Label>
+                  ),
+              )}
+            </fieldset>
 
-        <Link to="/">Voltar</Link>
-      </Container>
-    </>
+            <fieldset>
+              <legend>Individual</legend>
 
+              {plan
+                .filter(
+                  individuaPlan => individuaPlan.servicePlan === 'Individual',
+                )
+                .map(p =>
+                  p.workout === checkIndividualPlans(p) ? (
+                    <Label key={p.workout}>
+                      <Input
+                        name={p.workout}
+                        type="checkbox"
+                        onClick={handleValue}
+                        defaultChecked
+                      />
+                      {p.workout}
+                    </Label>
+                  ) : (
+                    <Label key={p.workout}>
+                      <Input
+                        name={p.workout}
+                        type="checkbox"
+                        onClick={handleValue}
+                      />
+                      {p.workout}
+                    </Label>
+                  ),
+                )}
+            </fieldset>
+          </GymPlan>
+          <Label>
+            <Input
+              name="account"
+              type="checkbox"
+              defaultChecked={userFound.account}
+            />
+            Conta ativa ?
+          </Label>
+          <PlanValue
+            basicValue={basicValue}
+            individualValue={individualValue}
+          />
+          <Button type="submit">Enviar</Button>
+        </Form>
+      ) : (
+        <h2>{userNotFound}</h2>
+      )}
+      <Link to="/">Voltar</Link>
+    </Container>
   );
-};
-export default ChangeData;
+}
--- src/pages/ChangeData/style.ts
+++ src/pages/ChangeData/style.ts
@@ -1,113 +1,111 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  justify-content:center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-  h2{
-    margin:8px;
+  h2 {
+    margin: 8px;
   }
 
-  a{
-    text-decoration:none;
-    text-transform:uppercase;
-    color:var(--text-border);
+  a {
+    text-decoration: none;
+    text-transform: uppercase;
+    color: var(--text-border);
 
-    border-radius:8px;
+    border-radius: 8px;
 
-    margin:8px auto;
-    padding:8px;
-    background-color:orange;
+    margin: 8px auto;
+    padding: 8px;
+    background-color: orange;
     transition: background 0.3s;
 
-    &:hover{
-    background-color:orangered;
+    &:hover {
+      background-color: orangered;
     }
   }
 `;
 
 export const SearchContent = styled.div`
-  color:var(--text-border);
+  color: var(--text-border);
 
-  display:flex;
+  display: flex;
 
-  margin:32px auto;
+  margin: 32px auto;
 
-  input{
-    margin:0;
-    padding:8px;
+  input {
+    margin: 0;
+    padding: 8px;
 
-    border:none;
-    border-radius:0.5em 0 0 0.5em;
+    border: none;
+    border-radius: 0.5em 0 0 0.5em;
   }
 
-  button{
-    padding:12px;
-    border-radius:0 0.5em 0.5em 0;
+  button {
+    padding: 12px;
+    border-radius: 0 0.5em 0.5em 0;
     cursor: pointer;
 
-    font-weight:700;
+    font-weight: 700;
     font-family: var(--custom-font);
-    color:var(--text-border);
-    background-color:orange;
+    color: var(--text-border);
+    background-color: orange;
     transition: background 0.3s;
 
-    &:hover{
-    background-color:orangered;
+    &:hover {
+      background-color: orangered;
     }
   }
 
-  svg{
-    font-size:32px;
+  svg {
+    font-size: 32px;
   }
-
 `;
 
 export const Form = styled.form`
-display:flex;
-flex-direction:column;
-align-items:center;
-justify-content:center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-input{
-  margin:6px;
-  padding:16px;
-  border:none;
-  border-radius:0.5em ;
-}
-
-  svg{
-    font-size:32px;
+  input {
+    margin: 6px;
+    padding: 16px;
+    border: none;
+    border-radius: 0.5em;
   }
 
-  button{
-    font-weight:bolder;
+  svg {
+    font-size: 32px;
+  }
+
+  button {
+    font-weight: bolder;
 
     transition: background 0.3s;
 
-    &:hover{
-    background-color:#D5D8DC;
+    &:hover {
+      background-color: #d5d8dc;
     }
   }
 `;
 
 export const GymPlan = styled.div`
-display:flex;
+  display: flex;
 
+  fieldset {
+    margin: 8px;
+    padding: 16px;
 
-  fieldset{
-    margin:8px;
-    padding:16px;
-
-    legend{
-      display:flex;
+    legend {
+      display: flex;
     }
   }
 `;
 
 export const Label = styled.label`
-  display:flex;
-  align-items:center;
+  display: flex;
+  align-items: center;
 `;
