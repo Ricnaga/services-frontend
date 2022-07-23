8bf990b647e45cc480a9163913a483c7e57f8221
Ricnaga <ric.nagato@gmail.com>
2022-07-23 20:02:46 -0300

    feat: add faker e iniciado msw mock

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { mockServer } from './application/mocks/msw/rest';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const root = createRoot(document.getElementById('app') as HTMLElement);
 
+async function createMockedServer() {
+  return process.env.NODE_ENV === 'development' && mockServer.start();
+}
+
+root.render(<App />);
+
+createMockedServer();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
