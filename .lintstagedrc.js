13aac3c34235398f0083d740260d282b8bd9ec7e
Ricnaga <ric.nagato@gmail.com>
2022-07-24 14:03:08 -0300

    test: Testes de screen e components simples

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.ts?(x)': ['yarn lint:fix'],
   '*.{js,jsx,ts,tsx,json,css}': ['prettier --write'],
-  '*.spec.ts?(x)': ['yarn test --bail -u'],
+  '*.spec.ts?(x)': ['yarn test:snap --bail'],
 };
